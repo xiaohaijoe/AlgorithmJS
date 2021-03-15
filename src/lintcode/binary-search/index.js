a2c569cac780cc908f997c8297b7fa2b227ab398
hijack <xiaohaijoe@hotmail.com>
2021-03-15 13:09:13 +0800

    binary search

--- src/lintcode/binary-search/index.js
+++ src/lintcode/binary-search/index.js
@@ -1,31 +1,64 @@
 // 457. 经典二分查找问题
 // 描述
 // 在一个排序数组中找一个数，返回该数出现的任意位置，如果不存在，返回 -1。
-class Solution457  {
-    binarySearch(nums, target) {
-        if(!nums || nums.length === 0) {
-            return -1;
-        }
-
-        let start = 0, end = nums.length - 1;
-        let mid = 0;
-        while(start + 1 < end) {
-            mid = start + parseInt((end - start) / 2);
-            if(nums[mid] === target) {
-                return mid;
-            } else if(nums[mid] > target) {
-                end = mid;
-            } else {
-                start = mid;
-            }
-        }
-
-        if(nums[start] === target) {
-            return start;
-        }
-        if(nums[end] === target) {
-            return end;
-        }
-        return -1;
+class Solution457 {
+  binarySearch(nums, target) {
+    if (!nums || nums.length === 0) {
+      return -1;
     }
+
+    let start = 0,
+      end = nums.length - 1;
+    let mid = 0;
+    while (start + 1 < end) {
+      mid = start + parseInt((end - start) / 2);
+      if (nums[mid] === target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        end = mid;
+      } else {
+        start = mid;
+      }
+    }
+
+    if (nums[start] === target) {
+      return start;
+    }
+    if (nums[end] === target) {
+      return end;
+    }
+    return -1;
+  }
+}
+
+// 14. 二分查找
+// 给定一个排序的整数数组（升序）和一个要查找的整数target，用O(logn)的时间查找到target第一次出现的下标（从0开始），如果target不存在于数组中，返回-1。
+class Solution14 {
+  binarySearch(nums, target) {
+    if (!nums || nums.length === 0) {
+      return -1;
+    }
+
+    let start = 0,
+      end = nums.length - 1;
+    let mid = 0;
+    while (start + 1 < end) {
+      mid = start + parseInt((end - start) / 2);
+
+      if (nums[mid] === target) {
+        end = mid;
+      } else if (nums[mid] > target) {
+        end = mid;
+      } else {
+        start = mid;
+      }
+    }
+    if (nums[start] === target) {
+      return start;
+    }
+    if (nums[end] === target) {
+      return end;
+    }
+    return -1;
+  }
 }
