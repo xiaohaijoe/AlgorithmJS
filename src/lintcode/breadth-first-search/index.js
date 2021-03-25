12141f909bc898e6981333e0e1e9733e6cf9b699
hijack <xiaohaijoe@hotmail.com>
2021-03-25 18:33:23 +0800

    commit breadth first search

--- src/lintcode/breadth-first-search/index.js
+++ src/lintcode/breadth-first-search/index.js
@@ -49,4 +49,48 @@ class Solution69 {
   }
 }
 
-Solution69.test();
+class Solution11 {
+  deserialize(data) {
+    // write your code here
+    if (data == null || data.length <= 2) {
+      return null;
+    }
+    console.log(data);
+    const substring = data.substring(1, data.length - 1);
+    console.log(substring)
+    const strs = substring.split(",");
+
+    // TreeNode root = new TreeNode(0);
+    const head = {};
+    this.helper(root, strs, 0, 1);
+    return root.right;
+  }
+
+  helper(root, strs, nextStart, nextEnd) {
+    let i = nextStart;
+    // console.log(nextStart, nextEnd, strs.length);
+    while (i < nextEnd && i < strs.length) {
+      const val = strs[i];
+      const left = i % 2 == 1;
+      console.log(val, left, i, strs);
+      if (val == "#") {
+      } else {
+        const node = { val: parseInt(val) };
+        if (left) {
+          root.left = node;
+        } else {
+          root.right = node;
+        }
+        this.helper(node, strs, nextEnd, 2 * nextEnd + 1);
+      }
+      i++;
+    }
+  }
+  static test() {
+      const solution = new Solution11();
+      const root = "{3,1,2,#,5}";
+      const res = solution.deserialize(root);
+      console.log(res);
+  }
+}
+Solution11.test();
