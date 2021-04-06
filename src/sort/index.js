cae305d6ca34942b7d04ecf6d0076ae6373735d8
xiaohaijoe <xiaohaijoe@hotmail.com>
2021-04-07 00:13:16 +0800

    sort

--- src/sort/index.js
+++ src/sort/index.js
@@ -46,7 +46,7 @@ class SolutionQuickSort {
 }
 
 // 快速排序 Version 2
-class SolutionQuickSort {
+class SolutionQuickSort2 {
   sortList(list) {
     if (list == null || list.length <= 1) {
       return list;
@@ -61,7 +61,6 @@ class SolutionQuickSort {
     }
     const index = start + parseInt((end - start) / 2);
     const pivot = list[index];
-
 
     this.quickList(list, start, right);
     this.quickList(list, left, end);
@@ -122,4 +121,81 @@ class SolutionInsertionSort {
   }
 }
 
-SolutionInsertionSort.test();
+class SolutionQS {
+  sortList(A) {
+    this.quickSort(A, 0, A.length - 1);
+    return A;
+  }
+  quickSort(A, start, end) {
+    if (start >= end) {
+      return;
+    }
+
+    const pivot = A[start + parseInt((end - start) / 2)];
+    let left = start;
+    let right = end;
+    while (left <= right) {
+      while (left <= right && A[left] < pivot) {
+        left++;
+      }
+      while (left <= right && A[right] > pivot) {
+        right--;
+      }
+      if (left <= right) {
+        const temp = A[left];
+        A[left] = A[right];
+        A[right] = temp;
+
+        left++;
+        right--;
+      }
+    }
+
+    this.quickSort(A, start, right);
+    this.quickSort(A, left, end);
+  }
+  static test() {
+    const solution = new SolutionQS();
+    const arr = [5, 2, 3, 1, 6, 4];
+    const res = solution.sortList(arr);
+    console.log(res);
+  }
+}
+
+class SolutionQS2 {
+  sortList(A) {
+    this.quickSort(A, 0, A.length - 1);
+    return A;
+  }
+  quickSort(A, left, right) {
+    if (left >= right) {
+      return;
+    }
+    const pivot = A[right];
+    let loc = left - 1;
+    for (let i = left; i < A.length - 1; i++) {
+      if (A[i] < pivot) {
+        loc++;
+        this.swap(A, i, loc);
+      }
+    }
+
+    loc++;
+    this.swap(A, right, loc);
+
+    this.quickSort(A, left, loc - 1);
+    this.quickSort(A, loc + 1, right);
+  }
+  swap(A, i1, i2) {
+    const temp = A[i1];
+    A[i1] = A[i2];
+    A[i2] = temp;
+  }
+  static test() {
+    const solution = new SolutionQS2();
+    const arr = [5, 2, 3, 1, 6, 4];
+    const res = solution.sortList(arr);
+    console.log(res);
+  }
+}
+SolutionQS2.test();
